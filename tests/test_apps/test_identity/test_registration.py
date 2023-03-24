30832c4b99de3b9cdcf5690fae7e59f2bc721fb7
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2023-03-24 13:24:58 +0900

    Test missing required field during registration

--- tests/test_apps/test_identity/test_registration.py
+++ tests/test_apps/test_identity/test_registration.py
@@ -7,30 +7,56 @@ import pytest
 from server.apps.identity.models import User
 
 
+pytestmark = pytest.mark.django_db
+
+
 if TYPE_CHECKING:
-    from tests.plugins.identity.user import ProfileData, ProfileAssertion
+    from tests.plugins.identity.user import ProfileAssertion, ProfileData
 
 
-@pytest.mark.django_db
-def test_valid_registration(
-    client: Client,
+@pytest.fixture
+def registration_data(
     user_email: str,
     user_password: str,
     user_profile_data: 'ProfileData',
-    assert_user_profile: 'ProfileAssertion',
-) -> None:
-    auth_data = {
+):
+    return user_profile_data | {
         'email': user_email,
         'password1': user_password,
         'password2': user_password,
     }
-    response = client.post(
-        '/identity/registration',
-        data=user_profile_data | auth_data,
-    )
+
+
+def test_valid_registration(
+    client: Client,
+    user_email: str,
+    user_password: str,
+    registration_data,
+    assert_user_profile: 'ProfileAssertion',
+) -> None:
+    response = client.post('/identity/registration', data=registration_data)
     assert response.status_code == HTTPStatus.FOUND, (
         response.context['form'].errors
     )
     user = User.objects.all().get(email=user_email)
     assert user.check_password(user_password)
-    assert_user_profile(user, user_profile_data)
+    assert_user_profile(user, registration_data)
+
+
+@pytest.mark.parametrize(
+    'missing_field',
+    User.REQUIRED_FIELDS + [User.USERNAME_FIELD],
+)
+def test_registration_missing_required_field(
+    client: Client,
+    user_email: str,
+    registration_data,
+    missing_field: str,
+) -> None:
+    response = client.post(
+        '/identity/registration',
+        data=registration_data | {missing_field: ''},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert missing_field in response.context['form'].errors
+    assert not User.objects.filter(email=user_email).exists()
