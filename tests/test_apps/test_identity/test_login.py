d633e72f16c8899770e1cb7a4609bff75f22e407
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2023-03-25 15:07:30 +0900

    Add `test_inactive_user_login`

--- tests/test_apps/test_identity/test_login.py
+++ tests/test_apps/test_identity/test_login.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import TYPE_CHECKING
 
 import pytest
 from django.test.client import Client
@@ -8,7 +9,15 @@ from server.apps.identity.models import User
 pytestmark = pytest.mark.django_db
 
 
-def test_successful_login(client: Client, user: User, user_password: str):
+if TYPE_CHECKING:
+    from tests.plugins.identity.user import UserFactory
+
+
+def test_valid_credentials_login(
+    client: Client,
+    user: User,
+    user_password: str,
+):
     """Providing valid username & password leads to successful login."""
     request_data = {'username': user.email, 'password': user_password}
     response = client.post('/identity/login', data=request_data)
@@ -16,3 +25,15 @@ def test_successful_login(client: Client, user: User, user_password: str):
         response.context['form'].errors
     )
     assert response['location'] == '/pictures/dashboard'
+
+
+def test_inactive_user_login(
+    client: Client,
+    user_factory: UserFactory,
+    user_password: str,
+):
+    user = user_factory(password=user_password, is_active=False)
+    request_data = {'username': user.email, 'password': user_password}
+    response = client.post('/identity/login', data=request_data)
+    assert response.status_code == HTTPStatus.OK
+    assert response.context['form'].errors['__all__']
