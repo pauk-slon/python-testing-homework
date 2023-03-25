0f9e40470e5dde23c84f70a5400225a2139c16b2
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2023-03-25 18:08:44 +0900

    Make `_assert_user_profile_correct` nested

--- tests/plugins/identity/user.py
+++ tests/plugins/identity/user.py
@@ -68,25 +68,23 @@ def user_profile_data(
 ProfileAssertion = Callable[[User, ProfileData], None]
 
 
-def _assert_user_profile_correct(user: User, profile_data: ProfileData):
-    assert user.first_name == profile_data['first_name']
-    assert user.last_name == profile_data['last_name']
-    if user.date_of_birth:
-        formatted_date_of_birth = user.date_of_birth.strftime(
-            USER_BIRTHDAY_FORMAT,
-        )
-        assert formatted_date_of_birth == profile_data['date_of_birth']
-    else:
-        assert not profile_data['date_of_birth']
-    assert user.address == profile_data['address']
-    assert user.job_title == profile_data['job_title']
-    assert user.phone == profile_data['phone']
-
-
 @pytest.fixture()
 def assert_user_profile_correct() -> ProfileAssertion:
     """Checks if `ProfileData` corresponds to the given `User` instance."""
-    return _assert_user_profile_correct
+    def factory(user: User, profile_data: ProfileData):
+        assert user.first_name == profile_data['first_name']
+        assert user.last_name == profile_data['last_name']
+        if user.date_of_birth:
+            formatted_date_of_birth = user.date_of_birth.strftime(
+                USER_BIRTHDAY_FORMAT,
+            )
+            assert formatted_date_of_birth == profile_data['date_of_birth']
+        else:
+            assert not profile_data['date_of_birth']
+        assert user.address == profile_data['address']
+        assert user.job_title == profile_data['job_title']
+        assert user.phone == profile_data['phone']
+    return factory
 
 
 @pytest.fixture()
