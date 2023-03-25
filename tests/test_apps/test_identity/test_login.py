166692b20189a39004cc24df1a144a0ec71e2b9e
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2023-03-25 15:21:27 +0900

    Add `test_invalid_credentials_login`

--- tests/test_apps/test_identity/test_login.py
+++ tests/test_apps/test_identity/test_login.py
@@ -29,12 +29,33 @@ def test_valid_credentials_login(
 
 def test_inactive_user_login(
     client: Client,
-    user_factory: UserFactory,
+    user_factory: 'UserFactory',
     user_password: str,
 ):
     """An inactive user should fail when loging in."""
     user = user_factory(password=user_password, is_active=False)
     request_data = {'username': user.email, 'password': user_password}
+    response = client.post('/identity/login', data=request_data)
+    assert response.status_code == HTTPStatus.OK
+    assert response.context['form'].errors['__all__']
+
+
+@pytest.mark.parametrize(
+    'invalid_fields',
+    [['username'], ['password'], ['username', 'password']],
+)
+def test_invalid_credentials_login(
+    client: Client,
+    user: User,
+    user_password: str,
+    invalid_fields: list[str],
+):
+    """An inactive user should fail when loging in."""
+    request_data = {'username': user.email, 'password': user_password}
+    for invalid_field in invalid_fields:
+        request_data[invalid_field] = (
+            '{0}-invalid'.format(request_data[invalid_field])
+        )
     response = client.post('/identity/login', data=request_data)
     assert response.status_code == HTTPStatus.OK
     assert response.context['form'].errors['__all__']
